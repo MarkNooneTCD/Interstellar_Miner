bd43ed0105cd3f6696ec2e0a687cb2ebbff36dde
MarkNooneTCD <manoone@tcd.ie>
2018-04-14 23:35:41 +0100

    Added resource drop

--- public/interstellar.js
+++ public/interstellar.js
@@ -1,6 +1,6 @@
 // Necessary Game constants
 const mvmtSpeed = 5;
-const fireRate = 100;
+const fireRate = 300;
 const gameWidth = window.innerWidth;
 const gameHeight = window.innerHeight;
 const mapWidth = 5000;
@@ -9,7 +9,7 @@ const meteorChildSpawnMax = 3;
 const meteorChildSpawnMin = 1;
 const asteroidOffset = 4;
 // Create Groups
-let asteroids;
+let asteroids, resources;
 
 // Create sprite variables
 let fighter ;
@@ -33,6 +33,7 @@ function preload() {
     Interstellar.load.image('bullet', 'assets/images/bullet.png');
     Interstellar.load.image('space', 'assets/images/space.jpg');
     Interstellar.load.image('asteroid', 'assets/images/asteroid.png');
+    Interstellar.load.image('gold', 'assets/images/resources/gold.png');
     Interstellar.load.image('health-100', 'assets/images/health/100-bar.png');
     Interstellar.load.image('health-90', 'assets/images/health/90-bar.png');
     Interstellar.load.image('health-80', 'assets/images/health/80-bar.png');
@@ -51,13 +52,17 @@ function create() {
     Interstellar.physics.startSystem(Phaser.Physics.ARCADE);
     // Interstellar.physics.startSystem(Phaser.Physics.P2JS);
     fireButton = Interstellar.input.activePointer.leftButton;
+    // resources.physicsBodyType = Phaser.Physics.ARCADE;
+    // resources.enableBody = true;
 
     this.spaceBackground = Interstellar.add.tileSprite(0, 0, mapWidth, mapHeight, 'space');
     Interstellar.world.setBounds(0, 0, mapWidth, mapHeight);
 
+    resources = Interstellar.add.group();
+
     // Create the fighter ship
     fighter = Interstellar.add.sprite(400, 300, 'fighter');
-    // fighter = Interstellar.add.sprite(Interstellar.world.centerX, Interstellar.world.centerY, 'fighter');
+
     fighter.anchor.setTo(0.5, 0.5);
     fighter.scale.setTo(.4, .4);
 
@@ -112,15 +117,22 @@ function createAsteroids(){
       spawnTmp.anchor.setTo(0.5, 0.5);
 
       spawn.events.onKilled.add(() => {
-        let r = Math.floor(Math.random() * 3)+1;
+        let r = 1;//Math.floor(Math.random() * 3)+1;
+        let resource;
         switch(r){
           case 1:
+            resource = Interstellar.add.sprite(spawn.x, spawn.y, 'gold');
+            resource.scale.setTo(1.3, 1.3);
+            resource.anchor.setTo(0.5, 0.5);
+            Interstellar.physics.arcade.enable(resource);
+            resource.body.setCircle(6, 0, 0);
+            resources.add(resource);
             break;
           case 2:
             break;
           case 3:
             break;
-          default: 
+          default:
         }
       });
 
@@ -137,11 +149,19 @@ function update() {
       item.getChildAt(0).angle += .5;
       Interstellar.physics.arcade.overlap(bullets, item, asteroidCollisionHandler, null, this);
     });
+    Interstellar.physics.arcade.overlap(fighter, resources, resourceHandler, null, this);
+    resources.forEachAlive((item)=>{
+      item.angle += -.5;
+    });
     movementWASD();
     if (fireButton.isDown)
     {
         fireBullet();
     }
+}
+
+function resourceHandler(fighter, resource) {
+  resource.kill();
 }
 
 function asteroidCollisionHandler (asteroid, bullet) {
@@ -179,6 +199,10 @@ function damageAsteroid(asteroid, damage){
 function render() {
     Interstellar.debug.spriteInfo(fighter, 32, 32);
     Interstellar.debug.body(asteroids);
+    resources.forEachAlive((item)=>{
+      Interstellar.debug.body(item);
+    });
+
 }
 
 function fireBullet () {
