d7a28014b2db3ff36f836a1d7156b945e12b7716
MarkNooneTCD <manoone@tcd.ie>
2018-04-21 22:07:40 +0100

    Added debug comments

--- server.js
+++ server.js
@@ -13,27 +13,26 @@ io.on('connection',function(socket){
 
     socket.on('newplayer',function(username){
         socket.player = {
-            id: server.lastPlayderID++,
+            id: server.lastPlayderID,
             username: username,
-            x: randomInt(100,400),
-            y: randomInt(100,400)
+            x: 100,
+            y: 150*(server.lastPlayderID++)
         };
         socket.emit('allplayers', getAllPlayers());
         socket.broadcast.emit('newplayer', socket.player);
 
-        socket.on('coord',function(data){
-            // console.log('click to '+data.x+', '+data.y);
-            socket.player.x = data.x;
-            socket.player.y = data.y;
-            io.emit('move',socket.player);
-        });
 
         socket.on('disconnect',function(){
             io.emit('remove',socket.player.id);
+        });
 
+        socket.on('coord',function(data){
+            console.log("Coord Recieved: "+data.dataX + " " + data.dataY);
+            socket.player.x = data.dataX;
+            socket.player.y = data.dataY;
+            io.emit('move',socket.player);
         });
     });
-
 });
 
 function getAllPlayers(){
